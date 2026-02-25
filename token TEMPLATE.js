c0322ede865b42a87d867dde3114c1beb3b19f5a
Your Name <you@example.com>
2026-02-25 12:42:24 -0500

    added image aliases

--- token TEMPLATE.js
+++ token TEMPLATE.js
@@ -1,4 +1,12 @@
+// https://id.atlassian.com/manage-profile/security/api-tokens
 const apiToken = ""
 const domain = "*.atlassian.net"
 const email = "*@*.*"
 const currentProject = "T1"
+// initials as keys
+const imageReplaces = {
+  AA: "./images/sm.png",
+  AB: "./images/notes.png",
+  BA: "./images/programer.png",
+  BB: "./images/qa.png",
+}
