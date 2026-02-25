4de374e6541f6cab53847f5e68dc2422644d13ac
Your Name <you@example.com>
2026-02-25 12:53:37 -0500

    removed need for email

--- token TEMPLATE.js
+++ token TEMPLATE.js
@@ -1,7 +1,6 @@
 // https://id.atlassian.com/manage-profile/security/api-tokens
 const apiToken = ""
 const domain = "*.atlassian.net"
-const email = "*@*.*"
 const currentProject = "T1"
 // initials as keys
 const imageReplaces = {
