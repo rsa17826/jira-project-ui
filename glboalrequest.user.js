fccbe65b8b5087ea0e2bae53f7a80d9e3bd96d64
Your Name <you@example.com>
2026-02-25 11:29:49 -0500

    added assignee image

--- glboalrequest.user.js
+++ glboalrequest.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         globalrequest
-// @version      1
+// @version      2
 // @description  none
 // @run-at       document-start
 // @author       rssaromeo
@@ -19,6 +19,7 @@ function c() {
       GM_xmlhttpRequest(data)
       await waituntil(() => temp)
       temp.text = temp.responseText
+      temp.ok = temp.status == 200
       return temp
     }
   }
