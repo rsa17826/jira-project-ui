57e4be6d4f947747a3f820afecc7956460dde140
Your Name <you@example.com>
2026-03-03 11:54:27 -0500

    a

--- token TEMPLATE.js
+++ token TEMPLATE.js
@@ -1,8 +1,9 @@
 // https://id.atlassian.com/manage-profile/security/api-tokens
 const apiToken = ""
+// even with api key it appears that you must be logged in to jira
 const domain = "*.atlassian.net"
 const currentProject = "T1"
-// required only if not logged into jira it appears
+// doesnt appear to require being set before connecting to jira
 var email = "*@*.*"
 // initials as keys
 const imageReplaces = {
